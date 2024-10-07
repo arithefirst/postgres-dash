d61c70e323f78d16d11b48d875dde64a51c3a8ae
April Hall <ari@arithefirst.com>
2024-10-07 11:10:20 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <h1 class="text-2xl text-center font-bold">Welcome to SvelteKit</h1>
+    <p class="text-center">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
