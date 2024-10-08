aebb743f1688f32d937915bf36922a082dcac867
April Hall <ari@arithefirst.com>
2024-10-08 13:19:09 -0400

    Convert SQL data into HTML table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,27 @@
-<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-    <h1 class="text-2xl text-center font-bold">Welcome to SvelteKit</h1>
-    <p class="text-center">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
+<script lang="ts">
+   // Get the contents of the table
+   let sqlResponse = fetchData()
+    async function fetchData() {
+        let response = await fetch('/api/db');
+        return await response.json();
+    }
+</script>
+
+<table>
+    <tr>
+        <th>id</th>
+        <th>email</th>
+        <th>gender</th>
+        <th>firstname</th>
+        <th>lastname</th>
+    </tr>
+    {#await sqlResponse then data}
+        {#each data as entry}
+            <tr>
+                {#each Object.entries(entry) as [,value]}
+                    <th>{value}</th>
+                {/each}
+            </tr>
+        {/each}
+    {/await}
+</table>
