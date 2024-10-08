c8bb1890abc0ce148172eb4ecd89a0a1856896c1
April Hall <ari@arithefirst.com>
2024-10-08 11:02:32 -0400

    Move query api into query dir

--- src/routes/api/db/query/[field]/[query]/+server.ts
+++ src/routes/api/db/query/[field]/[query]/+server.ts
@@ -1,5 +1,5 @@
 import {json} from "@sveltejs/kit";
-import sql from "../../sql";
+import sql from "../../../sql";
 
 export async function GET({ params }) {
     const db = await sql`SELECT * FROM users WHERE ${sql(params.field)} = ${params.query}`;
