6fa832e53d515f9d6c1f408a62777776618fb822
April Hall <ari@arithefirst.com>
2024-10-08 08:44:00 -0400

    Create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    const error = $page.error!.message;
+</script>
+
+<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <h1 class="text-5xl text-center font-bold">{$page.status}</h1>
+    <p class="text-center">{error}</p>
+</div>
