20cabbbd65fc903e5f37eeeb166fe48bd9d89d89
April Hall <ari@arithefirst.com>
2024-10-09 13:40:43 -0400

    Style table with DaisyUI and TailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,28 +15,30 @@
     }
 </script>
 
-<div class="overflow-x-auto w-11/12 border-black border-2 rounded-lg absolute left-1/2 top-8 mb-8 -translate-x-1/2">
-    <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-        <thead class="bg-gray-200">
-        <tr>
-            {#await cols then data}
-                {#each data as col}
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">{col}</th>
-                {/each}
-            {/await}
-        </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-200">
-        {#await sqlResponse then data}
-            {#each data as entry}
-                <tr class="hover:bg-gray-50 focus:outline-none transition duration-300">
-                    {#each Object.entries(entry) as [,value]}
-                        <td class="px-6 py-3 whitespace-no-wrap text-sm text-gray-900">{value}</td>
-                    {/each}
+<div class="overflow-x-auto w-11/12 absolute left-1/2 -translate-x-1/2 top-4">
+    <div class="rounded-xl border-2 border-base-300 overflow-scroll">
+        <table class="table table-zebra rounded-xl">
+            <thead>
+                <tr>
+                    {#await cols then data}
+                        {#each data as col}
+                            <th class="text-base-content">{col}</th>
+                        {/each}
+                    {/await}
                 </tr>
-            {/each}
-        {/await}
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                {#await sqlResponse then data}
+                    {#each data as entry}
+                        <tr>
+                            {#each Object.entries(entry) as [,value]}
+                                <td>{value}</td>
+                            {/each}
+                        </tr>
+                    {/each}
+                {/await}
+            </tbody>
+        </table>
+    </div>
+    <div class="h-4"/>
 </div>
-
