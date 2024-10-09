3bbcb0b4d19a091add600da771831eaba6f62b61
April Hall <ari@arithefirst.com>
2024-10-09 00:17:26 -0400

    Make html table adjust to fit different relations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
+    const table = "login"
+
    // Get the contents of the table
-   let sqlResponse = fetchData()
+    let sqlResponse = fetchData()
     async function fetchData() {
-        let response = await fetch('/api/db');
+        let response = await fetch(`/api/db/all/${table}`);
+        return await response.json();
+    }
+
+    let cols = fetchCols()
+    async function fetchCols() {
+        let response = await fetch(`/api/db/cols/${table}`);
         return await response.json();
     }
 </script>
 
 <table>
     <tr>
-        <th>id</th>
-        <th>email</th>
-        <th>gender</th>
-        <th>firstname</th>
-        <th>lastname</th>
+        {#await cols then data}
+            {#each data as col}
+                <th>{col}</th>
+            {/each}
+        {/await}
     </tr>
     {#await sqlResponse then data}
         {#each data as entry}
